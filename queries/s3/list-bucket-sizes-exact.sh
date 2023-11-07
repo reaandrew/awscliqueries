b49a0355acb7acca19dd7f84ed4b85443a0caa42
Andy Rea <email@andrewrea.co.uk>
2023-11-07 20:10:08 +0000

    Update the output of list bucket sizes exact

--- queries/s3/list-bucket-sizes-exact.sh
+++ queries/s3/list-bucket-sizes-exact.sh
@@ -5,5 +5,5 @@ set -e
 aws s3api list-buckets | jq -r '.Buckets[] | .Name' | \
 while read -r bucketName;
 do
-  echo "$bucketName,$(aws s3 ls "s3://$bucketName" --recursive --summarize --human-readable)"
+  echo "$bucketName,$(aws s3 ls "s3://$bucketName" --recursive --summarize --human-readable | grep "Total Size" | cut -d: -f2)"
 done
