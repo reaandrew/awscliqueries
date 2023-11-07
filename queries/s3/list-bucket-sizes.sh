4300a19ca6c5128c20aeb0cd79e9728844962109
Andy Rea <email@andrewrea.co.uk>
2023-11-07 19:47:50 +0000

    Add a query to list bucket sizes

---
+++ queries/s3/list-bucket-sizes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+aws s3api list-buckets | jq -r '.Buckets[] | .Name' | \
+while read -r bucketName;
+do
+  size=$(aws cloudwatch get-metric-statistics --namespace AWS/S3 \
+    --start-time $(date -d '1 month ago' +%Y-%m-%dT%H:%M:%SZ) \
+    --end-time $(date +%Y-%m-%dT%H:%M:%SZ) \
+    --period 31536000 \
+    --statistics Average \
+    --region eu-west-2 \
+    --metric-name BucketSizeBytes \
+    --dimensions Name=BucketName,Value="$bucketName" Name=StorageType,Value=StandardStorage \
+    --output json)
+
+    size_in_bytes=$(jq 'if .Datapoints == [] then 0 else .Datapoints[0].Average end' <<< "$size")
+    echo -e "$bucketName,$(echo "scale=2; $size_in_bytes / (1024 * 1024 * 1024)" | bc) GB"
+done
