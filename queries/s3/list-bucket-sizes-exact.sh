d64c86f9f85315f051af4ff496d0a6533c328424
Andy Rea <email@andrewrea.co.uk>
2023-11-07 20:08:49 +0000

    Add list bucket sizes exact

---
+++ queries/s3/list-bucket-sizes-exact.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+aws s3api list-buckets | jq -r '.Buckets[] | .Name' | \
+while read -r bucketName;
+do
+  echo "$bucketName,$(aws s3 ls "s3://$bucketName" --recursive --summarize --human-readable)"
+done
