c6f139d6ef246f6a217d4223e09895ec7be26d8f
Andy Rea <email@andrewrea.co.uk>
2023-11-09 13:19:47 +0000

    Updating to handle those with 1 history item

--- queries/ec2/ec2-instances-uptime.sh
+++ queries/ec2/ec2-instances-uptime.sh
@@ -9,19 +9,20 @@ aws ec2 describe-instances --filter 'Name=instance-state-name,Values=running' \
   --output text | \
     while read -r instance_id; do
       previous_state=""
+      uptime='unknown'
       while read -r event_time state; do
         state=${state//\'/}
+        event_time_seconds=$(date -d "${event_time//\'/}" +%s)
+        uptime=$(( (current_time - event_time_seconds) / 3600 ))
         if [[ -z "$previous_state" ]]; then
           previous_state="$state"
         fi
         if [[ "$previous_state" != "running" && "$state" == "running" ]]; then
-            event_time_seconds=$(date -d "${event_time//\'/}" +%s)
-            uptime=$(( (current_time - event_time_seconds) / 3600 ))
-            echo "$instance_id,$uptime"
             break
         fi
         previous_state="$state"
       done < <(aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 100 |
                        jq -r -c '.configurationItems[] | [.configurationItemCaptureTime, (.configuration | fromjson | .state.name)] | @sh')
+      echo "$instance_id,$uptime"
     done
 
