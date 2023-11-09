7f4ff1b39a230f0b9d99ca34c2c5c6adf951eebe
Andy Rea <email@andrewrea.co.uk>
2023-11-09 13:22:10 +0000

    Updated parallel version

--- queries/ec2/parallel-ec2-instances-uptime.sh
+++ queries/ec2/parallel-ec2-instances-uptime.sh
@@ -6,13 +6,22 @@ set -e
 function get_uptime(){
   instance_id=$1
   current_time=$2
-  aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 1 |
-      jq -r -c '.configurationItems[] | [.configurationItemCaptureTime] | @sh' | \
-      while read -r event_time; do
-        event_time_seconds=$(date -d "${event_time//\'/}" +%s)
-        hours_diff=$(( (current_time - event_time_seconds) / 3600 ))
-        echo "$instance_id,$hours_diff"
-      done
+  previous_state=""
+  uptime='unknown'
+  while read -r event_time state; do
+    state=${state//\'/}
+    event_time_seconds=$(date -d "${event_time//\'/}" +%s)
+    uptime=$(( (current_time - event_time_seconds) / 3600 ))
+    if [[ -z "$previous_state" ]]; then
+      previous_state="$state"
+    fi
+    if [[ "$previous_state" != "running" && "$state" == "running" ]]; then
+        break
+    fi
+    previous_state="$state"
+  done < <(aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 100 |
+                   jq -r -c '.configurationItems[] | [.configurationItemCaptureTime, (.configuration | fromjson | .state.name)] | @sh')
+  echo "$instance_id,$uptime"
 }
 
 export -f get_uptime
