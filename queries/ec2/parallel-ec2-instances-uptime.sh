c90a7877262d960c8ea3e4910619f0d0e26411a6
Andy Rea <email@andrewrea.co.uk>
2023-11-09 12:16:22 +0000

    Adding current time

--- queries/ec2/parallel-ec2-instances-uptime.sh
+++ queries/ec2/parallel-ec2-instances-uptime.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-current_time=$(date +%s)
 
 function get_uptime(){
   instance_id=$1
+  current_time=$2
   aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 1 |
       jq -r -c '.configurationItems[] | select((.configuration | fromjson | .state.name)=="running") | [.configurationItemCaptureTime] | @sh' | \
       while read -r event_time; do
@@ -17,7 +17,9 @@ function get_uptime(){
 
 export -f get_uptime
 
+current_time=$(date +%s)
+
 aws ec2 describe-instances --filter 'Name=instance-state-name,Values=running' \
   --query 'Reservations[*].Instances[*].[InstanceId]' \
   --output text | sed 's/"//g' | \
-    parallel --will-cite --jobs 10 --colsep ',' get_uptime
+    parallel --will-cite --jobs 5 --colsep ',' get_uptime {} "$current_time"
