f9c25a973ea72d84a10e2e1066f9f6731c1ad94e
Andy Rea <email@andrewrea.co.uk>
2023-11-09 13:14:28 +0000

    Fixing the script to loop at last state too

--- queries/ec2/ec2-instances-uptime.sh
+++ queries/ec2/ec2-instances-uptime.sh
@@ -8,11 +8,20 @@ aws ec2 describe-instances --filter 'Name=instance-state-name,Values=running' \
   --query 'Reservations[*].Instances[*].[InstanceId]' \
   --output text | \
     while read -r instance_id; do
-      aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 1 |
-        jq -r -c '.configurationItems[] | select((.configuration | fromjson | .state.name)=="running") | [.configurationItemCaptureTime] | @sh' | \
-        while read -r event_time; do
-          event_time_seconds=$(date -d "${event_time//\'/}" +%s)
-          hours_diff=$(( (current_time - event_time_seconds) / 3600 ))
-          echo "$instance_id,$hours_diff"
-        done
-      done
+      previous_state=""
+      while read -r event_time state; do
+        state=${state//\'/}
+        if [[ -z "$previous_state" ]]; then
+          previous_state="$state"
+        fi
+        if [[ "$previous_state" != "running" && "$state" == "running" ]]; then
+            event_time_seconds=$(date -d "${event_time//\'/}" +%s)
+            uptime=$(( (current_time - event_time_seconds) / 3600 ))
+            echo "$instance_id,$uptime"
+            break
+        fi
+        previous_state="$state"
+      done < <(aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 100 |
+                       jq -r -c '.configurationItems[] | [.configurationItemCaptureTime, (.configuration | fromjson | .state.name)] | @sh')
+    done
+
--- queries/ec2/parallel-ec2-instances-uptime.sh
+++ queries/ec2/parallel-ec2-instances-uptime.sh
@@ -7,7 +7,7 @@ function get_uptime(){
   instance_id=$1
   current_time=$2
   aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 1 |
-      jq -r -c '.configurationItems[] | select((.configuration | fromjson | .state.name)=="running") | [.configurationItemCaptureTime] | @sh' | \
+      jq -r -c '.configurationItems[] | [.configurationItemCaptureTime] | @sh' | \
       while read -r event_time; do
         event_time_seconds=$(date -d "${event_time//\'/}" +%s)
         hours_diff=$(( (current_time - event_time_seconds) / 3600 ))
