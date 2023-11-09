4b698d246081a0e8deb3d59992fc62e6a24a9671
Andy Rea <email@andrewrea.co.uk>
2023-11-09 08:53:20 +0000

    Fetch instances first

--- queries/ec2/ec2-instances-uptime.sh
+++ queries/ec2/ec2-instances-uptime.sh
@@ -4,10 +4,13 @@ set -e
 
 current_time=$(date +%s)
 
-aws configservice get-resource-config-history --resource-type AWS::EC2::Instance --limit 1 |
-  jq -c '.configurationItems[] | select(.resourceType=="AWS::EC2::Instance" and .configuration.state.name=="running") | [.resourceId,.configurationItemCaptureTime] | @sh' | \
-  while read -r instance_id event_time; do
-    event_time_seconds=$(date -d "$event_time" +%s)
-    hours_diff=$(( (current_time - event_time_seconds) / 3600 ))
-    echo "$instance_id, $hours_diff"
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' | \
+while read -r instance_id; do
+  aws configservice get-resource-config-history --resource-type AWS::EC2::Instance --limit 1 |
+    jq -c '.configurationItems[] | select(.resourceType=="AWS::EC2::Instance" and .configuration.state.name=="running") | [.configurationItemCaptureTime] | @sh' | \
+    while read -r event_time; do
+      event_time_seconds=$(date -d "$event_time" +%s)
+      hours_diff=$(( (current_time - event_time_seconds) / 3600 ))
+      echo "$instance_id, $hours_diff"
+    done
   done
