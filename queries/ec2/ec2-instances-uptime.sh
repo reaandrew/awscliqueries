a71a036ae8ed791f55ba3cd57da064ecc8869169
Andy Rea <email@andrewrea.co.uk>
2023-11-09 11:35:24 +0000

    Add raw output for jq

--- queries/ec2/ec2-instances-uptime.sh
+++ queries/ec2/ec2-instances-uptime.sh
@@ -9,7 +9,7 @@ aws ec2 describe-instances --filter 'Name=instance-state-name,Values=running' \
   --output text | \
     while read -r instance_id; do
       aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 1 |
-        jq -c '.configurationItems[] | select((.configuration | fromjson | .state.name)=="running") | [.configurationItemCaptureTime] | @sh' | \
+        jq -r -c '.configurationItems[] | select((.configuration | fromjson | .state.name)=="running") | [.configurationItemCaptureTime] | @sh' | \
         while read -r event_time; do
           event_time_seconds=$(date -d "${event_time//\'/}" +%s)
           hours_diff=$(( (current_time - event_time_seconds) / 3600 ))
