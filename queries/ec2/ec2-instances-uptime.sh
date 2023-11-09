386d53f40566d383a4d1dca506c15470b27a9318
Andy Rea <email@andrewrea.co.uk>
2023-11-09 11:34:04 +0000

    Update the script with various fixes

--- queries/ec2/ec2-instances-uptime.sh
+++ queries/ec2/ec2-instances-uptime.sh
@@ -4,13 +4,15 @@ set -e
 
 current_time=$(date +%s)
 
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' | \
-while read -r instance_id; do
-  aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 1 |
-    jq -c '.configurationItems[] | select(.resourceType=="AWS::EC2::Instance" and .configuration.state.name=="running") | [.configurationItemCaptureTime] | @sh' | \
-    while read -r event_time; do
-      event_time_seconds=$(date -d "$event_time" +%s)
-      hours_diff=$(( (current_time - event_time_seconds) / 3600 ))
-      echo "$instance_id, $hours_diff"
-    done
-  done
+aws ec2 describe-instances --filter 'Name=instance-state-name,Values=running' \
+  --query 'Reservations[*].Instances[*].[InstanceId]' \
+  --output text | \
+    while read -r instance_id; do
+      aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 1 |
+        jq -c '.configurationItems[] | select((.configuration | fromjson | .state.name)=="running") | [.configurationItemCaptureTime] | @sh' | \
+        while read -r event_time; do
+          event_time_seconds=$(date -d "${event_time//\'/}" +%s)
+          hours_diff=$(( (current_time - event_time_seconds) / 3600 ))
+          echo "$instance_id,$hours_diff"
+        done
+      done
