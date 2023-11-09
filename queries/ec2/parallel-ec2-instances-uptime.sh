937251d076341bf74a710b6073a036964f97fe53
Andy Rea <email@andrewrea.co.uk>
2023-11-09 12:12:31 +0000

    Add parallel get ec2 instances uptime

---
+++ queries/ec2/parallel-ec2-instances-uptime.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+current_time=$(date +%s)
+
+function get_uptime(){
+  instance_id=$1
+  aws configservice get-resource-config-history --resource-id "$instance_id" --resource-type AWS::EC2::Instance --limit 1 |
+      jq -r -c '.configurationItems[] | select((.configuration | fromjson | .state.name)=="running") | [.configurationItemCaptureTime] | @sh' | \
+      while read -r event_time; do
+        event_time_seconds=$(date -d "${event_time//\'/}" +%s)
+        hours_diff=$(( (current_time - event_time_seconds) / 3600 ))
+        echo "$instance_id,$hours_diff"
+      done
+}
+
+export -f get_uptime
+
+aws ec2 describe-instances --filter 'Name=instance-state-name,Values=running' \
+  --query 'Reservations[*].Instances[*].[InstanceId]' \
+  --output text | sed 's/"//g' | \
+    parallel --will-cite --jobs 10 --colsep ',' get_uptime
